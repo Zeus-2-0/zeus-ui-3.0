ba06dd054a4af6548e98b052bdb717af115d4f56
vbalaji215-dev <vbalaji215@outlook.com>
2023-04-27 06:41:36 -0400

    ZEUS-267 Updated test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('zeus-ui-3.0 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Zeus UI 3.0 - zeus-ui-3.0');
   });
 });
