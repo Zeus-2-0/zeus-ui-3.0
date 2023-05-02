7ee3bb532ffabab39a17c7d85174ef37e4e91b82
vbalaji215-dev <vbalaji215@outlook.com>
2023-05-02 16:40:40 -0400

    ZEUS-268 created the components

--- src/app/zeus/zeus.module.ts
+++ src/app/zeus/zeus.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HttpClientModule} from "@angular/common/http";
+import {ZeusComponent} from "./zeus.component";
+import {TradingPartnerComponent} from "./trading-partner/trading-partner.component";
+import {TradingPartnerSearchComponent} from "./trading-partner/trading-partner-search/trading-partner-search.component";
+import {
+  TradingPartnerCockpitComponent
+} from "./trading-partner/trading-partner-search/trading-partner-cockpit/trading-partner-cockpit.component";
+import {TradingPartnerEditComponent} from "./trading-partner/trading-partner-edit/trading-partner-edit.component";
+import {
+  TradingPartnerResultsComponent
+} from "./trading-partner/trading-partner-search/trading-partner-results/trading-partner-results.component";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ZeusComponent,
+    TradingPartnerComponent,
+    TradingPartnerSearchComponent,
+    TradingPartnerCockpitComponent,
+    TradingPartnerResultsComponent,
+    TradingPartnerEditComponent
+  ],
+  exports:[
+    ZeusComponent,
+    TradingPartnerComponent,
+    TradingPartnerSearchComponent,
+    TradingPartnerCockpitComponent,
+    TradingPartnerResultsComponent,
+    TradingPartnerEditComponent
+  ],
   imports: [
+    RouterModule,
     CommonModule,
-    HttpClientModule
+    FormsModule
   ]
 })
 export class ZeusModule { }
